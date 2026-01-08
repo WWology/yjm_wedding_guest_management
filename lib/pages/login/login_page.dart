29cadc635b1015f961cfd37bffa32dc831997f3e
WW <yosuamartiansia@gmail.com>
2026-01-08 11:32:41 +0700

    refactored different layouts to be in login page

--- lib/pages/login/login_page.dart
+++ lib/pages/login/login_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:responsive_framework/responsive_framework.dart';
 
 import 'widgets/login_form.dart';
 
@@ -7,6 +8,38 @@ class LoginPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: LoginForm());
+    final isLargeScreen = ResponsiveBreakpoints.of(context).largerThan(TABLET);
+    return Scaffold(
+      body: isLargeScreen ? LargeScreenLoginPage() : SmallScreenLoginPage(),
+    );
+  }
+}
+
+class LargeScreenLoginPage extends StatelessWidget {
+  const LargeScreenLoginPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: .center,
+      children: [
+        Expanded(child: Placeholder()),
+        const SizedBox(width: 24),
+        Expanded(child: LoginForm()),
+      ],
+    );
+  }
+}
+
+class SmallScreenLoginPage extends StatelessWidget {
+  const SmallScreenLoginPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      child: Column(
+        children: [Placeholder(), const SizedBox(height: 24), LoginForm()],
+      ),
+    );
   }
 }
