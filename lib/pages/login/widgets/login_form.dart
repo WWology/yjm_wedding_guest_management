f5821d87f85c05874534863fc31da18cf28ed36c
WW <yosuamartiansia@gmail.com>
2026-01-08 11:36:51 +0700

    refactored loginForm to be it's own set

--- lib/pages/login/widgets/login_form.dart
+++ lib/pages/login/widgets/login_form.dart
@@ -1,23 +1,41 @@
 import 'package:flutter/material.dart';
-import 'package:responsive_framework/responsive_framework.dart';
-import 'package:yjm_wedding_guest_management/pages/login/widgets/small_screen_login_form.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
-import 'large_screen_login_form.dart';
+import '../../../modules/auth/auth.dart';
+import 'email_form_field.dart';
+import 'password_form_field.dart';
 
 class LoginForm extends StatelessWidget {
-  LoginForm({super.key});
-
-  final _formKey = GlobalKey<FormState>();
+  const LoginForm({super.key});
 
   @override
   Widget build(BuildContext context) {
-    final isLargeScreen = ResponsiveBreakpoints.of(context).largerThan(TABLET);
-
-    return Form(
-      key: _formKey,
-      child: isLargeScreen
-          ? LargeScreenLoginForm(formKey: _formKey)
-          : SmallScreenLoginForm(formKey: _formKey),
+    return SizedBox(
+      height: 300,
+      child: Form(
+        child: Column(
+          mainAxisSize: .min,
+          crossAxisAlignment: .center,
+          children: [
+            EmailFormField(),
+            const SizedBox(height: 16),
+            PasswordFormField(),
+            const SizedBox(height: 16),
+            Builder(
+              builder: (context) {
+                return FilledButton.icon(
+                  onPressed: () {
+                    final formState = Form.of(context);
+                  },
+                  label: const Text('Login'),
+                  icon: const Icon(Icons.login),
+                  style: ButtonStyle(),
+                );
+              },
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
