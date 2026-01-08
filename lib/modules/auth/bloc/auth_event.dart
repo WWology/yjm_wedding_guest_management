9d5a4aeb770dd6f4c0c5e0fea718264445ba3173
WW <yosuamartiansia@gmail.com>
2026-01-08 11:36:30 +0700

    add login event

--- lib/modules/auth/bloc/auth_event.dart
+++ lib/modules/auth/bloc/auth_event.dart
@@ -5,5 +5,10 @@ sealed class AuthEvent with _$AuthEvent {
   const factory AuthEvent.userSubscriptionRequested() =
       UserSubscriptionRequested;
 
+  factory AuthEvent.loginRequested({
+    required String email,
+    required String password,
+  }) = LoginRequested;
+
   const factory AuthEvent.logOutRequested() = LogOutRequested;
 }
