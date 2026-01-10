6313912e4a8dafa0edfd5fee69104c7230ab1095
WW <yosuamartiansia@gmail.com>
2026-01-10 09:20:24 +0700

    wrap root Material with BlocListener to trigger refresh when AuthState changes

--- lib/main.dart
+++ lib/main.dart
@@ -32,20 +32,25 @@ class App extends StatelessWidget {
       create: (_) =>
           AuthBloc(authService: authService)
             ..add(const .userSubscriptionRequested()),
-      child: MaterialApp.router(
-        routerConfig: router,
-        title: 'Flutter Demo',
-        theme: ThemeData(colorScheme: .fromSeed(seedColor: Colors.deepPurple)),
-        debugShowCheckedModeBanner: false,
-        builder: (context, child) => ResponsiveBreakpoints.builder(
-          breakpoints: [
-            const Breakpoint(start: 0, end: 599, name: MOBILE),
-            const Breakpoint(start: 600, end: 839, name: TABLET),
-            const Breakpoint(start: 840, end: 1199, name: DESKTOP),
-            const Breakpoint(start: 1200, end: 1599, name: 'Large'),
-            const Breakpoint(start: 1600, end: double.infinity, name: 'XL'),
-          ],
-          child: child!,
+      child: BlocListener<AuthBloc, AuthState>(
+        listener: (context, state) => router.refresh(),
+        child: MaterialApp.router(
+          routerConfig: router,
+          title: 'Flutter Demo',
+          theme: ThemeData(
+            colorScheme: .fromSeed(seedColor: Colors.deepPurple),
+          ),
+          debugShowCheckedModeBanner: false,
+          builder: (context, child) => ResponsiveBreakpoints.builder(
+            breakpoints: [
+              const Breakpoint(start: 0, end: 599, name: MOBILE),
+              const Breakpoint(start: 600, end: 839, name: TABLET),
+              const Breakpoint(start: 840, end: 1199, name: DESKTOP),
+              const Breakpoint(start: 1200, end: 1599, name: 'Large'),
+              const Breakpoint(start: 1600, end: double.infinity, name: 'XL'),
+            ],
+            child: child!,
+          ),
         ),
       ),
     );
