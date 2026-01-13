ff4569ff950a63bfd03f55690c5997417aecd850
WW <yosuamartiansia@gmail.com>
2026-01-14 00:15:30 +0700

    rename HomePage to GuestListPage

---
+++ lib/pages/guest_list/guest_list_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class GuestListPage extends StatelessWidget {
+  const GuestListPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(child: const Text("Guest List Page"));
+  }
+}
