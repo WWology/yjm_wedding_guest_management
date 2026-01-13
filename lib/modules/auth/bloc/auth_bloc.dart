c7a8cc257116ffa991d87ab598da13d588e3230d
WW <yosuamartiansia@gmail.com>
2026-01-13 15:27:29 +0700

    ignore flutter import for freezed lint

--- lib/modules/auth/bloc/auth_bloc.dart
+++ lib/modules/auth/bloc/auth_bloc.dart
@@ -1,5 +1,6 @@
 import 'dart:async';
 
+// ignore_for_file: avoid_flutter_imports
 import 'package:flutter/foundation.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
