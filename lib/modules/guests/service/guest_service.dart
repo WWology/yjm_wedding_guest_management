15405ba506d8fceeaff977fdee47c4a01389f122
WW <yosuamartiansia@gmail.com>
2026-01-20 10:50:54 +0700

    pagination logic done

--- lib/modules/guests/service/guest_service.dart
+++ lib/modules/guests/service/guest_service.dart
@@ -19,35 +19,71 @@ class GuestService {
               );
 
   Stream<List<Guest>> get first10Guests {
-    final guestsSnapshot = _guestsRef.limit(10).orderBy('name').snapshots();
+    final guestsSnapshot = _guestsRef.limit(11).orderBy('name').snapshots();
 
-    return guestsSnapshot.map(
-      (snapshot) => snapshot.docs.map((doc) {
-        if (doc == snapshot.docs.last) {
-          _nextCursor = doc;
-          _prevCursor = null;
-        }
-        return doc.data();
-      }).toList(),
-    );
+    return guestsSnapshot.map((snapshot) {
+      if (snapshot.docs.length > 10) {
+        _nextCursor = snapshot.docs.last;
+      } else {
+        _nextCursor = null;
+      }
+
+      return snapshot.docs.take(10).map((doc) => doc.data()).toList();
+    });
   }
+
+  Stream<List<Guest>> get next10Guests {
+    if (!hasNextPage) return Stream.value([]);
+    _prevCursor = _nextCursor;
+
+    final nextGuestsSnapshot = _guestsRef
+        .startAtDocument(_nextCursor!)
+        .limit(11)
+        .orderBy('name')
+        .snapshots();
+
+    return nextGuestsSnapshot.map((snapshot) {
+      final notLastPage = snapshot.docs.length > 10;
+      if (notLastPage) {
+        _nextCursor = snapshot.docs.last;
+      } else {
+        _nextCursor = null;
+      }
+
+      return snapshot.docs.take(10).map((doc) => doc.data()).toList();
+    });
+  }
+
+  Stream<List<Guest>> get previous10Guests {
+    if (isFirstPage) {
+      return first10Guests;
+    }
+
+    _nextCursor = _prevCursor;
+
+    final prevGuestsSnapshot = _guestsRef
+        .endBeforeDocument(_prevCursor!)
+        .limitToLast(11)
+        .orderBy('name')
+        .snapshots();
+
+    return prevGuestsSnapshot.map((snapshot) {
+      final notFirstPage = snapshot.docs.length > 10;
+      if (notFirstPage) {
+        _prevCursor = snapshot.docs[1];
+        return snapshot.docs.skip(1).map((doc) => doc.data()).toList();
+      } else {
+        _prevCursor = null;
+        return snapshot.docs.map((doc) => doc.data()).toList();
+      }
+    });
+  }
+
+  bool get isFirstPage => _prevCursor == null;
+  bool get hasNextPage => _nextCursor != null;
 
   Future<int?> get guestCount async {
     final res = await _guestsRef.count().get();
     return res.count;
   }
-
-  Stream<List<Guest>> get next10Guests => _guestsRef
-      .startAfterDocument(_nextCursor!)
-      .limit(10)
-      .orderBy('name')
-      .snapshots()
-      .map((snapshot) => snapshot.docs.map((doc) => doc.data()).toList());
-
-  Stream<List<Guest>> get previous10Guests => _guestsRef
-      .endBeforeDocument(_prevCursor!)
-      .limitToLast(10)
-      .orderBy('name')
-      .snapshots()
-      .map((snapshot) => snapshot.docs.map((doc) => doc.data()).toList());
 }
